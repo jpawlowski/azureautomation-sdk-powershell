317dcd9398cc284f5106956923c9bcbbe977ac7b
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2024-07-14 02:23:47 +0200

    refactoring

--- Scripts/Get-GitRepositoryVersionDetails.ps1
+++ Scripts/Get-GitRepositoryVersionDetails.ps1
@@ -1,3 +1,27 @@
+<#PSScriptInfo
+.VERSION 1.0.0
+.GUID 0b1b1b1b-0b1b-0b1b-0b1b-0b1b0b1b0b1b
+.AUTHOR Julian Pawlowski
+.COMPANYNAME Workoho
+#>
+
+<#
+.SYNOPSIS
+    Get the nearest historical release and pre-release tags for the current tag in a Git repository.
+.DESCRIPTION
+    This script gets the nearest historical release and pre-release tags for the current tag in a Git repository.
+    The script requires Git to be installed and in the PATH.
+.PARAMETER CurrentTag
+    The current tag to get the nearest historical release and pre-release tags for.
+.OUTPUTS
+    System.Collections.Hashtable
+    An ordered hashtable with the following keys:
+    - nearestReleaseTag: The nearest release tag.
+    - nearestPreReleaseTag: The nearest pre-release tag in case the current tag is a pre-release tag.
+#>
+
+#Requires -Version 7.2
+
 [CmdletBinding()]
 [OutputType([System.Collections.Hashtable])]
 param(
@@ -5,7 +29,34 @@ param(
     [string]$CurrentTag
 )
 
+try {
+    $null = [semver]::new($CurrentTag -replace '^v')
+}
+catch {
+    Write-Error "Invalid version tag: $CurrentTag" -ErrorAction Stop
+    exit 1
+}
+
+try {
+    $null = git --version
+}
+catch {
+    Write-Error "Git is not installed or not in the PATH" -ErrorAction Stop
+    exit 1
+}
+
 $tags = git tag --list "v*" --sort=-version:refname
+
+if ($tags.Count -eq 0) {
+    Write-Error "No version tags found in the repository" -ErrorAction Stop
+    exit 1
+}
+
+if ($tags -notcontains $CurrentTag) {
+    Write-Error "Current tag '$CurrentTag' not found in the repository" -ErrorAction Stop
+    exit 1
+}
+
 $nearestReleaseTag = $null
 $nearestPreReleaseTag = $null
 $reachedCurrentTag = $false
@@ -29,8 +80,11 @@ foreach ($tag in $tags) {
         catch { continue }
 
         if ($version.PreReleaseLabel) {
-            if ($null -eq $nearestPreReleaseTag) {
-                Write-Verbose "Found nearest pre-release tag: $tag"
+            if (
+                $currentIsPreRelease -and
+                $null -eq $nearestPreReleaseTag
+            ) {
+                Write-Verbose "Found nearest pre-release tag for current pre-release: $tag"
                 $nearestPreReleaseTag = $tag
             }
         }
@@ -44,7 +98,6 @@ foreach ($tag in $tags) {
 }
 
 return [ordered]@{
-    nearestReleaseTag     = $nearestReleaseTag
-    nearestPreReleaseTag  = $nearestPreReleaseTag
-    previousPreReleaseTag = $previousPreReleaseTag
+    nearestReleaseTag    = $nearestReleaseTag
+    nearestPreReleaseTag = $nearestPreReleaseTag
 }
